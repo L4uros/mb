0f2ec0fa063e29423061575cf4ec0f3144d94746
L4uros <aanallaurassilva@gmail.com>
2024-09-23 20:57:47 -0300

    Add files via upload

---
+++ NBAAnalytics/LAAnalytics-main/LAAnalytics/src/usa/com/nba/atletas/operacoesController.java
@@ -0,0 +1,22 @@
+package usa.com.nba.atletas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class operacoesController {
+	
+	@FXML
+	private TextField txtNome;
+	@FXML
+	private TextField txtSobrenome;
+	@FXML
+	private TextField txtPosicao;
+	@FXML
+	private TextField txtEquipe;
+	@FXML
+	private TextField txtIdade;
+	@FXML
+	private Button btnCadastrar;
+	
+}//
